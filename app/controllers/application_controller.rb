29d4f652d2a052e187b72c7e737074c970c7a8af
neb5562 <gnebieridzs@unisens.ge>
2022-05-31 10:56:18 +0400

    changed blog post template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActionController::RoutingError, :with => :render_404
-
+  @title = "dfg"
   def error_page
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
