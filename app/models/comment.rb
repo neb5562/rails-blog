00caa2a13ee2b3f1c0cefc6e46b66a2a96ef05c3
neb5562 <gnebieridzs@unisens.ge>
2022-06-03 10:26:09 +0400

    added strip to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,12 @@ class Comment < ApplicationRecord
   belongs_to :blog, :counter_cache => true
   has_many :likes
   validates :body, presence: true,  length: { minimum: 2, maximum: 555 }
-  validates :body, format: { without: /\s/ }
+  before_validation :strip_whitespace
 
+  private
+  
+  def strip_whitespace
+    self.body = self.body.strip unless self.body.nil?
+  end
+  
 end
