49aa1b674d941912e305e97a340303bdcd6b3bce
neb5562 <gnebieridzs@unisens.ge>
2022-06-05 21:03:40 +0400

    added links in blogs list

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,7 +9,8 @@ class Blog < ApplicationRecord
   validates :blog_title, presence: true,  length: { maximum: 255 }
   validates :blog_text, presence: true,  length: { maximum: 65000 }
   validates :blog_description, presence: true,  length: { maximum: 255 }
-  validate :atleast_one_is_checked
+  validates :categories, presence: true
+
   def next
     nxt ||= Blog.where("id > ?", id).where(active: true).first
     nxt
@@ -18,9 +19,5 @@ class Blog < ApplicationRecord
   def prev
     prv ||= Blog.where("id < ?", id).where(active: true).last
     prv
-  end
-
-  def atleast_one_is_checked
-    errors.add(:base, "Select at least one category") if self.categories.blank?
   end
 end
