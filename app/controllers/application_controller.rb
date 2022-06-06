c5f2ff40818848a6bf840e2b005607d5d6075dcd
neb5562 <gnebieridzs@unisens.ge>
2022-06-06 16:23:15 +0400

    added 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActionController::RoutingError, :with => :render_404
   def error_page
-    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => true
   end
 
   protected
