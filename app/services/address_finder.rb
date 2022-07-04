7745df59bc081689cb814654f91429e8a33e7ed3
neb5562 <gnebieridzs@unisens.ge>
2022-07-04 17:37:09 +0400

    addresses

---
+++ app/services/address_finder.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'uri'
+
+class AddressFinder
+  ADDRESS_ACCESS_KEY = "f80dd2c405560fac7a01ab1be20c5b81"
+  def initialize(query)
+    @query = query
+  end
+
+  def find_addresses
+    
+
+    uri = URI.parse("http://api.positionstack.com/v1/forward?access_key=#{ADDRESS_ACCESS_KEY}&query=#{@query}")
+    response = Net::HTTP.get_response(uri)
+
+    response.body
+
+  end
+end
