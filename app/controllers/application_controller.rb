197cc139a04ef0ed0ca9653a4aec4eeaef628ed6
neb5562 <gnebieridzs@unisens.ge>
2022-07-26 15:31:50 +0400

    some minor fixes, user posts link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  include ActionView::Helpers::CaptureHelper
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
