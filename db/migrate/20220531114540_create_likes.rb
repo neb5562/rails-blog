0e5d029eae3fe580a35bd3379661ed0506d85598
neb5562 <gnebieridzs@unisens.ge>
2022-07-26 17:09:27 +0400

    added cascading users relationship models

--- db/migrate/20220531114540_create_likes.rb
+++ db/migrate/20220531114540_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: {on_delete: :cascade}
       t.references :comment, null: true, default: nil
       t.references :post, null: true, default: nil
       t.timestamps
